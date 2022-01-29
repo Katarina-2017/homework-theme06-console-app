bad706ab9d57a753f4d760f6f780114d22246046
katzhukova <katzhukova@gmail.com>
2022-01-29 21:46:18 +0300

    Реализовала метод FileRead() чтения из файла с информацией о сотрудниках

--- HomeworkTheme06ConsoleApp/Program.cs
+++ HomeworkTheme06ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,21 @@ namespace HomeworkTheme06ConsoleApp
 {
     class Program
     {
+        static void FileRead()
+        {
+            using (StreamReader sr = new StreamReader("db.txt"))
+            {
+                string line;
+                Console.WriteLine($"{"ID"}\t{"Датa и время записи"}\t{" Ф.И.О.",20}\t{"Возраст"}\t{"Рост"}\t{"Датa рождения"}\t{"Место рождения"}");
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] employees = line.Split('#');
+                    Console.WriteLine($"{employees[0]}\t{employees[1]}\t{employees[2]}\t{employees[3]}\t{employees[4]}\t{employees[5]}\t{employees[6]}");
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Выберите один из вариантов работы: " +
@@ -17,7 +33,7 @@ namespace HomeworkTheme06ConsoleApp
 
             switch (userOption)
             {
-                case 1:break;
+                case 1:FileRead(); break;
                 case 2:break;
                 default:Console.WriteLine("Вы ввели некорректное значение");break;
             }
