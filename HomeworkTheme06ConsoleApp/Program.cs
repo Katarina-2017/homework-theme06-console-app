dd575483aed2e3f452134f773f15e9a3a27a9873
katzhukova <katzhukova@gmail.com>
2022-01-29 21:16:28 +0300

    Реализовала выбор пользователя в зависимости от введенного значения

--- HomeworkTheme06ConsoleApp/Program.cs
+++ HomeworkTheme06ConsoleApp/Program.cs
@@ -13,7 +13,14 @@ namespace HomeworkTheme06ConsoleApp
             Console.WriteLine("Выберите один из вариантов работы: " +
                               "\nвведите 1- чтобы вывести данные на экран" +
                               "\nвведите 2 - чтобы заполнить данные и добавить новую запись в конец файла");
+            byte userOption = byte.Parse(Console.ReadLine());
 
+            switch (userOption)
+            {
+                case 1:break;
+                case 2:break;
+                default:Console.WriteLine("Вы ввели некорректное значение");break;
+            }
             Console.ReadKey();
         }
     }
